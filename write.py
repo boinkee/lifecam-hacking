6e7a96b9e8137e584288db33fd6369b09af780a7
boinkee <testere.boinkee@gmail.com>
2026-08-04 15:59:48 +0300

    add write ability

--- write.py
+++ write.py
@@ -1,28 +1,57 @@
 #!/usr/bin/env python3
 import usb.core
 import usb.util
+import sys
+import time
+
+if len(sys.argv) != 2:
+    print(" gimme the adress!!!")
+    print(f"usage:   {sys.argv[0]} 0x[adress]")
+    sys.exit(1)
 
 dev = usb.core.find(idVendor=0x045e, idProduct=0x0770)
 if dev is None:
     raise RuntimeError("check device")
 
+if dev.is_kernel_driver_active(0):
+    dev.detach_kernel_driver(0)
+
 try:
     cfg = dev.get_active_configuration()
-    print("device already configured", cfg.bConfigurationValue)
+    print("device already configured:", cfg.bConfigurationValue)
 except usb.core.USBError as e:
     print("Device not configured", e)
     print("Set configuration")
     dev.set_configuration()
 
-bmRequestType = 0x40
-wIndex = 0x0000
-wValue = 0x0000
 
-print("start")
-for req in range(0x100):
-    try:
-        dev.ctrl_transfer(bmRequestType, req, wIndex, wValue, None, timeout=100)
-        print(f"bRequest=0x{req:02X}  seems good")
-    except usb.core.USBError as e:
-        print(f"not good: {e}")
-print ("done")
+bmRequestType = 0x40 # out command
+wIndex = 0x0000
+bRequest = 0x03
+input = "input.bin"
+chunk_size = 64
+try:
+    base = int(sys.argv[1], 0)
+except ValueError:
+    print("invalid adress!")
+    sys.exit(1)
+
+print(f"open file {input}")
+with open(input, "rb") as f:
+    data = f.read()
+total = len(data)
+cur_adress = 0
+
+print(f"base adress:0x{base:04X}")
+print(f"size:{total} bytes")
+
+while cur_adress < total:
+    writebytes = min(chunk_size, total - cur_adress)
+    chunk = data[cur_adress:cur_adress + writebytes]
+    wValue = (base + cur_adress) & 0xFFFF
+    dev.ctrl_transfer(bmRequestType, bRequest, wValue, wIndex, chunk)
+    cur_adress += writebytes
+    percent = cur_adress * 100 / total
+    print(f"\rwriting: %{percent:.1f} " f"({cur_adress}/{total} bytes) "f"adress :0x{wValue:04X}", end="", flush=True)
+    time.sleep(0.001)
+print(f"\nwrote {total}")
